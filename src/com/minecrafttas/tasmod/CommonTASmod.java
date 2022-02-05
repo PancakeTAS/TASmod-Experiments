27a412643eb21fdf7cf78afeb68ca661eff66c54
MCPfannkuchenYT <gamesupport@mgnet.work>
2022-02-05 22:06:05 +0100

    Load the custom tasmod server and client when launching their side

--- src/com/minecrafttas/tasmod/CommonTASmod.java
+++ src/com/minecrafttas/tasmod/CommonTASmod.java
@@ -1,6 +1,10 @@
 package com.minecrafttas.tasmod;
 
+import com.minecrafttas.tasmod.exceptions.ServerAlreadyRunningException;
+import com.minecrafttas.tasmod.networking.CustomTASmodServer;
+
 import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -29,6 +33,7 @@ public class CommonTASmod {
 	
 	/**
 	 * Initialize the TASmod
+	 * This will register this class to the event bus from Forge, so that future Forge events will be triggered in this class
 	 * 
 	 * IMPLEMENTATION NOTICE:
 	 * This is a forge event called before the game launch method is called x2
@@ -38,13 +43,38 @@ public class CommonTASmod {
 	@EventHandler
 	public void onInit(FMLInitializationEvent e) {
 		TASmod.LOGGER.debug("Common TASmod Init Phase");
+		/* Register the current class to the event bus from forge */
+		TASmod.LOGGER.debug("Registering the CommonTASmod to the event bus");
+		MinecraftForge.EVENT_BUS.register(this); // this will add the current class to the event bus from forge
+		TASmod.LOGGER.debug("CommonTASmod was registered to the event bus");
+	}
+	
+	/**
+	 * Initialize the TASmod on the launching integrated or dedicated server.
+	 * This will launch the custom TASmod server on a separate thread.
+	 * 
+	 * IMPLEMENTATION NOTICE:
+	 * net.minecraft.server.MinecraftServer.run()V
+	 */
+	public void onServerLaunch() {
+		TASmod.LOGGER.debug("Common TASmod Server Launch Phase");
+		/* Launch the custom server thread */
+		try {
+			TASmod.LOGGER.debug("Trying to create the custom tasmod server");
+			CustomTASmodServer.createServer(); // this will create a new thread in which it launches the server. It also throws an exception if the last server wasn't successfully shut down.
+			TASmod.LOGGER.debug("Successfully created the custom tasmod server without any unexpected issues");
+		} catch (ServerAlreadyRunningException exception) {
+			// Note: The loglevel is only 'warn', since this exception is not fatal and the server was still started.
+			TASmod.LOGGER.warn("Exception thrown trying to launch the custom TASmod server!");
+			TASmod.LOGGER.warn(exception);
+		}
 	}
 	
 	/**
 	 * Updates the TASmod at the start of a tick
 	 * 
 	 * IMPLEMENTATION NOTICE:
-	 * net.minecraft.server.MinecraftServer.tick()V
+	 * net.minecraft.server.MinecraftServer.run()V at net.minecraft.server.MinecraftServer.tick()V
 	 * 
 	 * @param mcserver Instance of Minecraft Server
 	 */
--- src/com/minecrafttas/tasmod/client/ClientTASmod.java
+++ src/com/minecrafttas/tasmod/client/ClientTASmod.java
@@ -2,11 +2,16 @@ package com.minecrafttas.tasmod.client;
 
 import com.minecrafttas.tasmod.CommonTASmod;
 import com.minecrafttas.tasmod.TASmod;
+import com.minecrafttas.tasmod.exceptions.ClientAlreadyRunningException;
+import com.minecrafttas.tasmod.networking.CustomTASmodClient;
 
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -30,14 +35,43 @@ public class ClientTASmod extends CommonTASmod {
 	}
 	
 	/**
-	 * Initialize the TASmod clientside
+	 * Initialize the TASmod clientside.
+	 * This will register this class to the event bus from Forge, so that future Forge events will be triggered in this class
 	 */
 	@EventHandler
 	public void onInit(FMLInitializationEvent e) {
 		TASmod.LOGGER.debug("Client TASmod Init Phase");
+		/* Register the current class to the event bus from forge */
+		TASmod.LOGGER.debug("Registering the ClientTASmod to the event bus");
+		MinecraftForge.EVENT_BUS.register(this); // this will add the current class to the event bus from forge
+		TASmod.LOGGER.debug("ClientTASmod was registered to the event bus");
 		super.onInit(e);
 	}
 
+	/**
+	 * Initialize the TASmod when connecting to a world.
+	 * This will launch the custom TASmod client on a separate thread.
+	 * 
+	 * IMPLEMENTATION NOTICE:
+	 * This is a forge event called after the client is loaded, but before it launches
+	 * 
+	 * @param e Connected to the server event
+	 */
+	@SubscribeEvent
+	public void onClientConnect(ClientConnectedToServerEvent e) {
+		TASmod.LOGGER.debug("TASmod Connect Server Phase");
+		/* Launch the custom client thread */
+		try {
+			TASmod.LOGGER.debug("Trying to create the custom tasmod client");
+			CustomTASmodClient.createClient(); // this will create a new thread in which it launches the client. It also throws an exception if the last client wasn't successfully shut down.
+			TASmod.LOGGER.debug("Successfully created the custom tasmod client without any unexpected issues");
+		} catch (ClientAlreadyRunningException exception) {
+			// Note: The loglevel is only 'warn', since this exception is not fatal and the client was still started.
+			TASmod.LOGGER.warn("Exception thrown trying to launch the custom TASmod client!");
+			TASmod.LOGGER.warn(exception);
+		}
+	}
+	
 	/**
 	 * Updates the TASmod at the start of a client tick
 	 * 
