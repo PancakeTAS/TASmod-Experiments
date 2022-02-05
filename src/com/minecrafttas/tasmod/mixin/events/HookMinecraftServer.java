39d3d063d615952e3332eb81d199d069a94158b4
MCPfannkuchenYT <gamesupport@mgnet.work>
2022-02-05 21:15:30 +0100

    Change the server tick event call to the loop instead of the tick
    Previously the tick would not be called with the integrated server paused

--- src/com/minecrafttas/tasmod/mixin/events/HookMinecraftServer.java
+++ src/com/minecrafttas/tasmod/mixin/events/HookMinecraftServer.java
@@ -19,9 +19,13 @@ public class HookMinecraftServer {
 
 	/**
 	 * Triggers an Event in {@link CommonTASmod#onServerTick(Minecraft)} before every tick
+	 * 
+	 * IMPLEMENTATION NOTICE:
+	 * The run() method is the run method for the entire server. This mixin catches when tick() is called and then triggers an event. It is very important
+	 * not to inject into the head of tick() since it can be cancelled by the integrated server while the server is paused (eg. the escape menu is open).
 	 * @param ci Callback Info
 	 */
-	@Inject(method = "tick", at = @At("HEAD"))
+	@Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;tick()V"))
 	public void hookRunTickEvent(CallbackInfo ci) {
 		CommonTASmod.instance.onServerTick((MinecraftServer) (Object) this);
 	}
